170ce0f8c179ccf71dc26884a063ae06c02d6804
LeynardChequito <cjohnleynard399@gmail.com>
2023-10-08 18:34:53 +0800

    Admin?

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,4 @@ $routes->get('/login', 'LoginController::login');
 $routes->get('/register', 'LoginController::register');
 $routes->post('/authreg', 'LoginController::authreg');
 $routes->post('/authlogin', 'LoginController::authlogin');
+$routes->get('/home', 'UserController::home');
---
+++ app/Views/admindash/footer.php
@@ -0,0 +1,19 @@
+<!-- inject:js -->
+<script src="<?php base_url(); ?> admindb/js/d3.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/getmdl-select.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/material.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/nv.d3.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/layout/layout.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/scroll/scroll.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/charts/discreteBarChart.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/charts/linePlusBarChart.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/charts/stackedBarChart.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/employer-form/employer-form.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/line-chart/line-charts-nvd3.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/map/maps.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/pie-chart/pie-charts-nvd3.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/table/table.min.js"></script>
+<script src="<?php base_url(); ?> admindb/js/widgets/todo/todo.min.js"></script>
+<!-- endinject -->
+
+</body>
